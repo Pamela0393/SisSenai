2f8436d5d36f156af6e6f32e73d0d5c998c6b09b
Pamela0393 <pab.0393@hotmail.com>
2026-06-09 19:22:07 -0300

    Update server.js

--- server.js
+++ server.js
@@ -79,7 +79,7 @@ app.put('/alterar-cliente/:id',(req,res)=>{
 app.delete('/excluir-cliente/:id', (req,res)=>{
 	const {id} = req.params;
 	db.run(`DELETE FROM clientes WHERE id = ? `[id], (err) => {
-		if (err) return res.status(500).json:err.message});
+		if (err) return res.status(500).json({err.message});
 	res.json({success:true});
 });
 });
